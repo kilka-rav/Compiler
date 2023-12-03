e067428ff04f69b97bea6ed8071a921f5256be56
kilka-rav <zakirianov.rv@phystech.edu>
2023-12-03 19:15:11 +0300

    DFS test

--- main.cpp
+++ main.cpp
@@ -4,57 +4,45 @@
 #include "operation.h"
 
 int main() {
-    
+
     std::string name = "Factorial";
     Module application(name);
     auto s = application.getName();
     application.setArgument("I32");
     application.setReturnType("I64");
     std::cout << application.getName() << std::endl;
-    auto b1 = BasicBlock(0, {0}, {1});
-    auto b2 = BasicBlock(1, {1}, {2, 3});
-    auto b3 = BasicBlock(2, {1}, {1});
-    auto b4 = BasicBlock(3, {3}, {3});
+    
+    auto b1 = BasicBlock(0, {0}, {1, 2, 3});
+    auto b2 = BasicBlock(1, {0}, {4, 5});
+    auto b3 = BasicBlock(2, {0}, {2});
+    auto b4 = BasicBlock(3, {0}, {6, 7});
+
+    auto b5 = BasicBlock(4, {1}, {8, 9});
+    auto b6 = BasicBlock(5, {1}, {5});
+
+    auto b7 = BasicBlock(6, {3}, {10, 11});
+    auto b8 = BasicBlock(7, {3}, {7});
+
+    auto b9 = BasicBlock(8, {4}, {8});
+    auto b10 = BasicBlock(9, {4}, {9});
+
+    auto b11 = BasicBlock(10, {6}, {10});
+    auto b12 = BasicBlock(11, {6}, {11});
     
     application.insert(&b1);
     application.insert(&b2);
     application.insert(&b3);
     application.insert(&b4);
 
-    
-    ConstantOperation c1 = ConstantOperation(0, 1, "I32");
-    ConstantOperation c2 = ConstantOperation(1, 2, "I32");
-    MoveOperation c13 = MoveOperation(2, "arg0");
-    JumpOperation c3 = JumpOperation(3, b2.getID());
-    b1.insert(&c1);
-    b1.insert(&c2);
-    b1.insert(&c13);
-    b1.insert(&c3);
-    
-    PhiOperation c4 = PhiOperation(4, b1.getID(), c2.getIndex(), b3.getID(), 9);
-    CompareOperation c5 = CompareOperation(5, "le", 4, 2);
-    PhiOperation c6 = PhiOperation(6, b1.getID(), c1.getIndex(), b3.getID(), 11);
-    IfOperation c7 = IfOperation(7, 2, 3, 5);
-    b2.insert(&c4);
-    b2.insert(&c5);
-    b2.insert(&c6);
-    b2.insert(&c7);
-    
-    ConstantOperation c8 = ConstantOperation(8, 1, "I32");
-    BinaryOperation c9 = BinaryOperation(9, "Add", c4.getIndex(), c8.getIndex());
-    CastOperation c10 = CastOperation(10, c9.getIndex(), "I64");
-    BinaryOperation c11 = BinaryOperation(11, "Mul", c6.getIndex(), c10.getIndex());
-    JumpOperation c12 = JumpOperation(12, b2.getID());
-    b3.insert(&c8);
-    b3.insert(&c9);
-    b3.insert(&c10);
-    b3.insert(&c11);
-    b3.insert(&c12);
-    
-    ReturnOperation c14 = ReturnOperation(13, c6.getIndex());
-    b4.insert(&c14);
-    
-    application.print();
+    application.insert(&b5);
+    application.insert(&b6);
+    application.insert(&b7);
+    application.insert(&b8);
+
+    application.insert(&b9);
+    application.insert(&b10);
+    application.insert(&b11);
+    application.insert(&b12);
 
     application.printDFS();
     application.DFS();
---
+++ tests/DFS.cpp
@@ -0,0 +1,79 @@
+#include <gtest/gtest.h>
+#include "./../include/module.h"
+#include "./../include/type.h"
+#include "./../include/operation.h"
+
+
+TEST(Marked, DFSTest) {
+/*
+id: 0 numDFS: -1 prev ids: 0  next ids: 1 2 3 
+id: 1 numDFS: -1 prev ids: 0  next ids: 4 5 
+id: 2 numDFS: -1 prev ids: 0  next ids: 2 
+id: 3 numDFS: -1 prev ids: 0  next ids: 6 7 
+id: 4 numDFS: -1 prev ids: 1  next ids: 8 9 
+id: 5 numDFS: -1 prev ids: 1  next ids: 5 
+id: 6 numDFS: -1 prev ids: 3  next ids: 10 11 
+id: 7 numDFS: -1 prev ids: 3  next ids: 7 
+id: 8 numDFS: -1 prev ids: 4  next ids: 8 
+id: 9 numDFS: -1 prev ids: 4  next ids: 9 
+id: 10 numDFS: -1 prev ids: 6  next ids: 10 
+id: 11 numDFS: -1 prev ids: 6  next ids: 11 
+After DFS
+id: 0 numDFS: 1 prev ids: 0  next ids: 1 2 3 
+id: 1 numDFS: 2 prev ids: 0  next ids: 4 5 
+id: 2 numDFS: 7 prev ids: 0  next ids: 2 
+id: 3 numDFS: 8 prev ids: 0  next ids: 6 7 
+id: 4 numDFS: 3 prev ids: 1  next ids: 8 9 
+id: 5 numDFS: 6 prev ids: 1  next ids: 5 
+id: 6 numDFS: 9 prev ids: 3  next ids: 10 11 
+id: 7 numDFS: 12 prev ids: 3  next ids: 7 
+id: 8 numDFS: 4 prev ids: 4  next ids: 8 
+id: 9 numDFS: 5 prev ids: 4  next ids: 9 
+id: 10 numDFS: 10 prev ids: 6  next ids: 10 
+id: 11 numDFS: 11 prev ids: 6  next ids: 11
+
+*/
+    std::string name = "DFS";
+    Module application(name);
+    auto s = application.getName();
+    application.setArgument("I32");
+    application.setReturnType("I64");
+    std::cout << application.getName() << std::endl;
+    
+    auto b1 = BasicBlock(0, {0}, {1, 2, 3});
+    auto b2 = BasicBlock(1, {0}, {4, 5});
+    auto b3 = BasicBlock(2, {0}, {2});
+    auto b4 = BasicBlock(3, {0}, {6, 7});
+
+    auto b5 = BasicBlock(4, {1}, {8, 9});
+    auto b6 = BasicBlock(5, {1}, {5});
+
+    auto b7 = BasicBlock(6, {3}, {10, 11});
+    auto b8 = BasicBlock(7, {3}, {7});
+
+    auto b9 = BasicBlock(8, {4}, {8});
+    auto b10 = BasicBlock(9, {4}, {9});
+
+    auto b11 = BasicBlock(10, {6}, {10});
+    auto b12 = BasicBlock(11, {6}, {11});
+    
+    application.insert(&b1);
+    application.insert(&b2);
+    application.insert(&b3);
+    application.insert(&b4);
+
+    application.insert(&b5);
+    application.insert(&b6);
+    application.insert(&b7);
+    application.insert(&b8);
+
+    application.insert(&b9);
+    application.insert(&b10);
+    application.insert(&b11);
+    application.insert(&b12);
+
+    //application.printDFS();
+    application.DFS();
+    std::cout << "After DFS" << std::endl;
+    //application.printDFS();
+}
